273d7e1cb7c620a118c8b31b5fe8f1824deb90be
Marcus <lamwc@vcu.edu>
2016-09-21 20:20:18 -0500

    API Almost Finished

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -6,8 +6,7 @@ var coordinates = [];
 var eventInfo = [];
 var contentString;
 var marker;
-
-
+var wLocation = 'Austin, TX';
 
 // Function that initializes the map and makes it appear on the page via callback.
 function initMap() {
@@ -19,6 +18,7 @@ function initMap() {
     });
     geocoder = new google.maps.Geocoder;
     infowindow = new google.maps.InfoWindow;
+    getWeather();
   //   var styledMapType = new google.maps.StyledMapType(
 		// [
 		// 	{
@@ -47,109 +47,125 @@ function initMap() {
   //   map.setMapTypeId('styled_map');
 };
 
-// Beginning of the click event that triggers the queries between the Meetup API and Google Maps API.
-$('#searchBtn').on('click', function() {
-	// var loading_screen = pleaseWait({
-	// 	logo: "assets/portfolio/pic.jpg",
-	// 	backgroundColor: '#f46d3b',
-	// 	loadingHtml: "<div class='sk-spinner sk-spinner-pulse'></div>",
-	// });
+$('.modal-content').on('keyup', function(event) {
+    if (event.keyCode == 13) {
+        $('#searchBtn').click();
+    }
+});
 
+// Beginning of the click event that triggers the queries between the Meetup API and Google Maps API.
+$(document).on('click', '#searchBtn, .collection-item', (function(event) {
 	// Clears data (from previous queries) from the arrays.
 	coordinates = [];
 	eventInfo = [];
-	// Stores user inputs into variables that are necessary for the Meetup API query.
-	var text = $('#textInput').val().trim();
-	var location = $('#locationInput').val().trim();
-	var amountOfResults = $('#resultsNumInput').val().trim();
+	var text;
+	var location;
+	var amountOfResults;
 
-    // Meetup API V3 query url that contains parameters which allow users to perform a raw full text and location query.
-    var queryURL = "https://api.meetup.com/find/groups?&sign=true&photo-host=public&filter=all&upcoming_events=true&fallback_suggestions=true&country=US&location="+location+"&radius=smart&text="+text+"&page="+amountOfResults+"&key=2b1e504a1c3ea586da4a465e6c5223";
-    // Additional features include:
-    // - Limiting the country to the U.S. and only displaying groups that have upcoming events.
-    // - A dynamic search radius based on the number of active groups in the area.
-    // - Returning a list of curated suggestions if criteria was not found.
-    // Beginning of the AJAX query event.
-    $.ajax({url: queryURL, method: 'GET', dataType: 'jsonp'}).done(function(response) {
-		// Stores the data returned from the Meetup API into the variable 'results'.
-		var results = response.data;
-		initMap();
+	if ($(event.target).is('#searchBtn')) {
+		// Stores user inputs into variables that are necessary for the Meetup API query.
+		text = $('#textInput').val().trim();
+		location = $('#locationInput').val().trim();
+		amountOfResults = $('#resultsNumInput').val().trim();
+	} else if ($(this).hasClass('collection-item')) {
+		text = $(this).children('.title').text();
+		location = 'Austin, TX';
+		amountOfResults = 30;
+		$('#modal1').closeModal();
+	}
 
-		for (var i=0; i<results.length; i++) {
-			// Pushes data gathered from the AJAX query into their associated arrays.
-			coordinates.push(results[i].lat + ', ' + results[i].lon);
-			eventInfo.push({
-				eventName: results[i].name,
-				eventDescription: results[i].description,
-				eventLink: results[i].link
-			});
-			// Concatenates objects from the 'eventInfo' array and sets it equal to the var 'contentString', which is then used within the 'infowindow' pop-up that appears whenever a map marker is clicked.
-			contentString = '<h1>' + eventInfo[i].eventName + '</h1>' + eventInfo[i].eventDescription + '<br><a href=' + eventInfo[i].eventLink + '>Meetup.com Event Link</a>';
-			// Calling the function defined below.
-			geocodeLatLng(map, marker, infowindow, contentString);
-			// Function that takes inputs and, in turn, creates map markers and infowindows accordingly.
-			function geocodeLatLng(map, marker, infowindow, contentString) {
-				var input = coordinates[i];
-			    var latlngStr = input.split(',', 2);
-			    var latlng = {
-			        lat: parseFloat(latlngStr[0]) + Math.random()*0.01,
-			        lng: parseFloat(latlngStr[1]) + Math.random()*0.01
-			    };
-	        	map.setCenter(latlng);
-                map.setZoom(10);
-                marker = new google.maps.Marker({
-                    map: map,
-                    position: latlng,
-                    animation: google.maps.Animation.DROP
-                });
-                infowindow = new google.maps.InfoWindow({
-					content: contentString,
+	if (text == "" || location == "") {
+		console.log("Required search fields are not met.");
+	} else {
+		wLocation = location;
+		getWeather();
+
+	    // Meetup API V3 query url that contains parameters which allow users to perform a raw full text and location query.
+	    var queryURL = "https://api.meetup.com/find/groups?&sign=true&photo-host=public&filter=all&upcoming_events=true&fallback_suggestions=true&country=US&location="+location+"&radius=smart&text="+text+"&page="+amountOfResults+"&key=2b1e504a1c3ea586da4a465e6c5223";
+	    // Additional features include:
+	    // - Limiting the country to the U.S. and only displaying groups that have upcoming events.
+	    // - A dynamic search radius based on the number of active groups in the area.
+	    // - Returning a list of curated suggestions if criteria was not found.
+	    // Beginning of the AJAX query event.
+	    $.ajax({url: queryURL, method: 'GET', dataType: 'jsonp'}).done(function(response) {
+			// Stores the data returned from the Meetup API into the variable 'results'.
+			var results = response.data;
+			initMap();
+
+			for (var i=0; i<results.length; i++) {
+				// Pushes data gathered from the AJAX query into their associated arrays.
+				coordinates.push(results[i].lat + ', ' + results[i].lon);
+				eventInfo.push({
+					eventName: results[i].name,
+					eventDescription: results[i].description,
+					eventLink: results[i].link
 				});
-                marker.addListener('click', function() {
-                	infowindow.open(map, marker);
-                });
+				// Concatenates objects from the 'eventInfo' array and sets it equal to the var 'contentString', which is then used within the 'infowindow' pop-up that appears whenever a map marker is clicked.
+				contentString = '<div class="iw-container"><div class="iw-title center">' + eventInfo[i].eventName + '</div><div class="main-text-padding"><div class="text-justify text-padding">' + eventInfo[i].eventDescription + '<br><a href=' + eventInfo[i].eventLink + '>Meetup.com Event Link</a></div></div></div>';
+				// Calling the function defined below.
+				geocodeLatLng(map, marker, infowindow, contentString);
+				// Function that takes inputs and, in turn, creates map markers and infowindows accordingly.
+				function geocodeLatLng(map, marker, infowindow, contentString) {
+					var input = coordinates[i];
+				    var latlngStr = input.split(',', 2);
+				    var latlng = {
+				        lat: parseFloat(latlngStr[0]) + Math.random()*0.01,
+				        lng: parseFloat(latlngStr[1]) + Math.random()*0.01
+				    };
+		        	map.setCenter(latlng);
+	                map.setZoom(12);
+	                marker = new google.maps.Marker({
+	                    map: map,
+	                    position: latlng,
+	                    animation: google.maps.Animation.DROP
+	                });
+	                infowindow = new google.maps.InfoWindow({
+						content: contentString
+					});
+	                marker.addListener('click', function() {
+	                	infowindow.open(map, marker);
+	                	if (marker.getAnimation() !== null) {
+							marker.setAnimation(null);
+						} else {
+							marker.setAnimation(google.maps.Animation.BOUNCE);
+						}
+						$(".gm-style-iw").siblings().children(':nth-child(2)').css({'display': 'none'});
+                   		$(".gm-style-iw").siblings().children(':nth-child(4)').css({'display': 'none'});
+                   		$(".gm-style-iw").siblings().children('img').parent().addClass("infoWindowClose");
+                    	$(".gm-style-iw").parent().addClass("infoWindowSize");
+	                });
+	                infowindow.addListener('closeclick', function() {
+	                	marker.setAnimation(null);
+	                });
+				};
 			};
-		};
-	}); // End of the AJAX query event.
+		}); // End of the AJAX query event.
+	};
+})); // End of the click event.
 
-	// setTimeout(loading_screen.finish.bind(loading_screen), 5000);
-	
-}); // End of the click event.
-
-
-
-$(document).ready(function() {
-
+function getWeather() {
 	$.simpleWeather({
-		location: 'Austin, TX',
+		location: wLocation,
 		unit: 'f',
+		success: function(weather) {
+	        html = '<img id="weatherImage" src='+weather.image+'>';
+	        html += "<p>Today's Weather</p>";
+	        html += '<div class="tempwrapper"><h3 id="currenttemp">'+weather.temp+'&deg;F</h3>';
+	        html += '<div class="smalltempwrapper"><p id="hightemp">'+"H: "+weather.high+'&deg;F</p><p id="lowtemp">'+"L: "+weather.low+'&deg;F</p></div></div>';
+	        html += '<ul><li id="weatherFullWidth">'+weather.city+', '+weather.region+'</li>';
+	        html += '<li id="weatherLeftHalf">'+weather.currently+'</li>';
+	        html += '<li id="weatherRightHalf">Humidity: '+weather.humidity+'%</li></ul>';
+	        html += '<div id="weatherForecast">'; 
+	        for (var i=0; i<5; i++) {
+	            html += '<img src=' + weather.forecast[i].thumbnail + '><p>' + weather.forecast[i].day + ': ' + weather.forecast[i].high + "&deg;F</p>";
+	        };
+	        html += '</div><div id="weatherLink"><a href="' + weather.link + '">Full Forecast Here</a></div>';
+	        html += '<div><p class="movedown valign-wrapper white-text">Last updated: '+ weather.updated +'</p></div>';
 
-	success: function(weather) {
-		html = '<img class="" src='+weather.image+'>';
-		html += "<p>Today's Weather</p>"
-		html += '<div class="tempwrapper"><h3 id="currenttemp">'+weather.temp+'&deg;'+weather.units.temp+'</h3>';
-		html += '<div class="smalltempwrapper"><p id="hightemp">'+ "H: " +weather.high+'&deg;F</p><p id="lowtemp">'+ "L: " +weather.low+'&deg;F</p></div></div>';
-
-		html += '<ul><li id="weatherFullWidth">'+weather.city+', '+weather.region+'</li>';
-		html += '<li id="weatherLeftHalf">'+weather.currently+'</li>';
-		html += '<li id="weatherRightHalf">Humidity: '+weather.humidity+'%</li></ul>';
-
-		html += '<p class="movedown valign-wrapper white-text">Last updated: '+ weather.updated +'</p>'
-		html += '<div id="thisisthething">'
-		// html += '<img class="logoimg" src="assets/images/logo.png">';
-		
-		for(var i=0;i<5;i++) {
-			html += '<img src=' + weather.forecast[i].thumbnail + '><p>' + weather.forecast[i].day + ':' + weather.forecast[i].high + "&deg;F</p>";
-		}
-
-		html += '</div><a id="weatherLink" href="' + weather.link + '">Full Forecast Here</a>';
-
-		$(".weather").html(html);
-
-	},
-
+			$(".weather").html(html);
+		},
 		error: function(error) {
 			$(".weather").html('<p>'+error+'</p>');
 		}
 	});
-});
+};
